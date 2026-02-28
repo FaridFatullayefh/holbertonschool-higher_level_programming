9ae4e508d31e3f7a83a0b7dcd0bbcd4bbbb9c55a
FaridFatullayefh <faridfatullayefh@gmail.com>
2026-02-28 15:57:29 +0400

    Create 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Bu modul obyektin atributlarını axtarmaq üçün funksiyanı ehtiva edir.
+"""
+
+
+def lookup(obj):
+    """Obyektin mövcud atribut və metodlarının siyahısını qaytarır.
+
+    Args:
+        obj: Yoxlanılacaq obyekt.
+
+    Returns:
+        list: Atribut və metodların adlarından ibarət siyahı.
+    """
+    return dir(obj)
