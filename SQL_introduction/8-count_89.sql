cf17fb7a0a87b48c2bb2a0e2f1616e7269087f14
FaridFatullayefh <faridfatullayefh@gmail.com>
2026-03-29 18:21:33 +0400

    Create 8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- 'first_table' cədvəlində id-si 89 olan sətirlərin sayını göstərir.
+-- Verilənlər bazasının adı komanda xəttində arqument kimi ötürülür.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
